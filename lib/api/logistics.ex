45f63d41bb648a789e2fe0aec9d626da054854ec
Ton <haquangtrong.ton@gmail.com>
2022-04-04 15:18:44 +0700

    Create api update shipping info

--- lib/api/logistics.ex
+++ lib/api/logistics.ex
@@ -32,6 +32,23 @@ defmodule TiktokShop.Logistics do
   end
 
   @doc """
+  Update shipping info
+
+  Reference: https://bytedance.feishu.cn/wiki/wikcnDCHk9oWB9iqsb58BX9Mlkf#qYtWHF
+  """
+  @update_shipping_info_schema %{
+    order_id: [type: :string, required: true],
+    tracking_number: [type: :string, required: true],
+    provider_id: [type: :string, required: true]
+  }
+  def update_shipping_info(params, opts \\ []) do
+    with {:ok, data} <- Contrak.validate(params, @update_shipping_info_schema),
+         {:ok, client} <- Client.new(opts) do
+      Client.post(client, "/api/logistics/tracking", data)
+    end
+  end
+
+  @doc """
   Get warehouse list
 
   Reference: https://bytedance.feishu.cn/wiki/wikcnDCHk9oWB9iqsb58BX9Mlkf#jN96M1
