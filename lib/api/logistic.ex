9692e59c44b44455922b6dce3d159db486d4b819
Ton <haquangtrong.ton@gmail.com>
2022-04-04 13:47:09 +0700

    Create api get shipping info

---
+++ lib/api/logistic.ex
@@ -0,0 +1,22 @@
+defmodule TiktokShop.Logistic do
+  @moduledoc """
+  Logistic API
+  """
+
+  alias TiktokShop.Client
+
+  @doc """
+  Get shipping info
+
+  Reference: https://bytedance.feishu.cn/wiki/wikcnDCHk9oWB9iqsb58BX9Mlkf#ckvNFO
+  """
+  @get_shipping_info_schema %{
+    order_id: [type: :string, required: true]
+  }
+  def get_shipping_info(params, opts \\ []) do
+    with {:ok, data} <- Contrak.validate(params, @get_shipping_info_schema),
+         {:ok, client} <- Client.new(opts) do
+      Client.get(client, "/api/logistics/ship/get", query: data)
+    end
+  end
+end
